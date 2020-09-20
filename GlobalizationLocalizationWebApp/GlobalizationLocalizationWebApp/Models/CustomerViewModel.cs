615e71fb6c044ef65024249ef67361d11d2d36e4
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-09-20 12:02:11 +0600

    Added model and resource files

---
+++ GlobalizationLocalizationWebApp/GlobalizationLocalizationWebApp/Models/CustomerViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlobalizationLocalizationWebApp.Models
+{
+    public class CustomerViewModel
+    {
+        [Display(Name = "FullName")]
+        public string FullName { get; set; }
+
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+        [Display(Name = "MobileNo")]
+        public string MobileNo { get; set; }
+    }
+}
---
+++ GlobalizationLocalizationWebApp/GlobalizationLocalizationWebApp/Resources/ApplicationResource.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlobalizationLocalizationWebApp.Resources
+{
+    public class ApplicationResource
+    {
+    }
+}
